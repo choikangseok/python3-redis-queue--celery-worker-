6a83b224da23819e667c86086b04135b1b509386
choikangseok <ktos5427@naver.com>
2022-03-21 11:08:41 +0900

    Create run_tasks.py

---
+++ project/run_tasks.py
@@ -0,0 +1,48 @@
+from tasks import *
+import csv,sys
+
+
+if __name__ == '__main__':
+        # Check parameter
+        # run_tasks.py <csv file>
+
+        if len(sys.argv) != 3:
+                print("Usage python run_tasks.py <input_file> <use_collections>")
+                exit(0)
+
+        input_file = sys.argv[1]
+        collection_num = int(sys.argv[2])
+        
+        
+        #수집 유형에 따른 다른 task를 실행하기 위함
+        if not collection_num in range(0, 4):
+                print("Collections num range in [0,1,2,3]")
+                exit(0)
+        file_name, file_ext = os.path.splitext(input_file)
+
+        # Check file extension
+        if file_ext not in '.csv':
+                print("Only input .csv format")
+                exit(0)
+
+        # Open file
+        파일을 읽어서 해당 데이터를 읽고 Queue 프로세스를 진행함
+        with open(input_file, 'r', encoding='utf-8-sig') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for i, row in enumerate(reader):
+                Objectid = row['_id']
+                First_value = row['_first_value']
+                Second_value = row['_second_value']
+                
+                #수집 유형에 따라 다른 task를 실행
+                if collection_num == 0:
+                    result = my_task_0.delay(Objectid, First_value, Second_value)
+                elif collection_num == 1:
+                    result = my_task_1.delay(Objectid, First_value, Second_value)
+                elif collection_num == 2:
+                    result = my_task_2.delay(Objectid, First_value, Second_value)
+                elif collection_num == 3:
+                    result = my_task_3.delay(Objectid, First_value, Second_value)
+                
+                  
+                print("[run_tasks.py]",result)
