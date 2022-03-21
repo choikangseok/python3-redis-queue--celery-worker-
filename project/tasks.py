79e664d25303c5db400b29d6a9e7b2d5df1a50da
choikangseok <ktos5427@naver.com>
2022-03-21 11:37:45 +0900

    Update tasks.py

--- project/tasks.py
+++ project/tasks.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from celery import Celery
-from wgetmanager import *
+from MongoWork import *
 from celery.exceptions import SoftTimeLimitExceeded
 import logging
 
@@ -9,6 +9,14 @@ app = Celery('tasks', backend='rpc://', broker='redis://localhost:6379')
 @app.task(bind=True, name='tasks.my_task_0.jobs', soft_time_limit=300)
 def my_task_0(self, Objectid, First_value, Second_value):
     try:
+        obj = MongoWork(Objectid, First_value, Second_value)
+        
+        #분석 시작 & download check 부분 DB업데이트
+        obj.run()
+
+        #전체 결과 DB 업데이트
+        obj.db_update()
+        
         pass
         #로직 정의 
         
