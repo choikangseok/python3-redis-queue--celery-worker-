13c8261b1b4ce1ddfc14eabf3dc0949fa06d3f88
choikangseok <ktos5427@naver.com>
2022-03-22 18:20:57 +0900

    MongoDB Handler

---
+++ project/DBHandler.py
@@ -0,0 +1,52 @@
+from config import *
+import logging
+from pymongo import MongoClient
+
+
+class DBHandler:
+    def __init__(self):
+        try:
+            self.client = MongoClient(host=MONGO_DB_IP,
+                                 port=MONGO_DB_PORT,
+                                 username=MONGO_DB_USERNAME,
+                                 password=MONGO_DB_PASSWORD,
+                                 authMechanism=MONGO_DB_AUTHM)
+        except Exception as e:
+            logging.error(f"[{self.__class__.__name__}] __init__ {e}")
+
+
+    def insert_item_one(self, data, db_name=None, collection_name=None):
+        result = self.client[db_name][collection_name].insert_one(data).inserted_id
+        return result
+
+    def insert_item_many(self, datas, db_name=None, collection_name=None):
+        result = self.client[db_name][collection_name].insert_many(datas).inserted_ids
+        return result
+
+    def find_item_one(self, condition=None, db_name=None, collection_name=None):
+        result = self.client[db_name][collection_name].find_one(condition, {"_id": False})
+        return result
+
+    def find_item(self, condition=None, db_name=None, collection_name=None):
+        result = self.client[db_name][collection_name].find(condition, {"_id": False}, no_cursor_timeout=True, cursor_type=CursorType.EXHAUST)
+        return result
+
+    def delete_item_one(self, condition=None, db_name=None, collection_name=None):
+        result = self.client[db_name][collection_name].delete_one(condition)
+        return result
+
+    def delete_item_many(self, condition=None, db_name=None, collection_name=None):
+        result = self.client[db_name][collection_name].delete_many(condition)
+        return result
+
+    def update_item_one(self, condition=None, update_value=None, db_name=None, collection_name=None):
+        result = self.client[db_name][collection_name].update_one(filter=condition, update=update_value)
+        return result
+
+    def update_item_many(self, condition=None, update_value=None, db_name=None, collection_name=None):
+        result = self.client[db_name][collection_name].update_many(filter=condition, update=update_value)
+        return result
+
+    def text_search(self, text=None, db_name=None, collection_name=None):
+        result = self.client[db_name][collection_name].find({"$text": {"$search": text}})
+        return result
--- project/tasks.py
+++ project/tasks.py
@@ -10,16 +10,16 @@ app = Celery('tasks', backend='rpc://', broker='redis://localhost:6379')
 def my_task_0(self, Objectid, First_value, Second_value):
     try:
         obj = MongoWork(Objectid, First_value, Second_value)
-        
+
         #분석 시작 & download check 부분 DB업데이트
         obj.run()
 
         #전체 결과 DB 업데이트
         obj.db_update()
-        
+
         pass
-        #로직 정의 
-        
+        #로직 정의
+
     except Exception as e:
         logging.error(f"[my_task_0], [{e}]")
     finally:
@@ -29,8 +29,8 @@ def my_task_0(self, Objectid, First_value, Second_value):
 def my_task_1(self, Objectid, First_value, Second_value):
     try:
         pass
-        #로직 정의 
-        
+        #로직 정의
+
     except Exception as e:
         logging.error(f"[my_task_1], [{e}]")
     finally:
@@ -40,19 +40,19 @@ def my_task_1(self, Objectid, First_value, Second_value):
 def my_task_2(self, Objectid, First_value, Second_value):
     try:
         pass
-        #로직 정의 
-        
+        #로직 정의
+
     except Exception as e:
         logging.error(f"[my_task_2], [{e}]")
     finally:
         return f"{obj.today} {obj.Objectid} finished!"
-        
+
 @app.task(bind=True, name='tasks.my_task_3.jobs', soft_time_limit=300)
 def my_task_3(self, Objectid, First_value, Second_value):
     try:
         pass
-        #로직 정의 
-        
+        #로직 정의
+
     except Exception as e:
         logging.error(f"[my_task_3], [{e}]")
     finally:
