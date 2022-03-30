b492c9b6df844a7f5fb22b857615f6e8204de82c
choikangseok <ktos5427@naver.com>
2022-03-31 01:25:17 +0900

    add unittest

--- project/run_tasks.py
+++ project/run_tasks.py
@@ -1,32 +1,29 @@
 from tasks import *
 import csv,sys
 
+def main(argv):
+    if len(argv) != 3:
+        print("Usage python run_tasks.py <input_file> <use_collections>")
+        exit(0)
 
-if __name__ == '__main__':
-        # Check parameter
-        # run_tasks.py <csv file>
-
-        if len(sys.argv) != 3:
-            print("Usage python run_tasks.py <input_file> <use_collections>")
-            exit(0)
-
-        input_file = sys.argv[1]
-        collection_num = int(sys.argv[2])
+    input_file = argv
+    collection_num = int(argv)
 
 
-        #수집 유형에 따른 다른 task를 실행하기 위함
-        if not collection_num in range(0, 4):
-            print("Collections num range in [0,1,2,3]")
-            exit(0)
-        file_name, file_ext = os.path.splitext(input_file)
+    #수집 유형에 따른 다른 task를 실행하기 위함
+    if not collection_num in range(0, 4):
+        print("Collections num range in [0,1,2,3]")
+        exit(0)
+    file_name, file_ext = os.path.splitext(input_file)
 
-        # Check file extension
-        if file_ext not in '.csv':
-            print("Only input .csv format")
-            exit(0)
+    # Check file extension
+    if file_ext not in '.csv':
+        print("Only input .csv format")
+        exit(0)
 
-        # Open file
-        파일을 읽어서 해당 데이터를 읽고 Queue 프로세스를 진행함
+    # Open file
+    #파일을 읽어서 해당 데이터를 읽고 Queue 프로세스를 진행함
+    try:
         with open(input_file, 'r', encoding='utf-8-sig') as csvfile:
             reader = csv.DictReader(csvfile)
             for i, row in enumerate(reader):
@@ -43,6 +40,13 @@ if __name__ == '__main__':
                     result = my_task_2.delay(Objectid, First_value, Second_value)
                 elif collection_num == 3:
                     result = my_task_3.delay(Objectid, First_value, Second_value)
+    except:
+        return False
+    else:
+        print("[run_tasks.py]",result)
+        return True
 
-
-                print("[run_tasks.py]",result)
+if __name__ == '__main__':
+    main(sys.argv)
+        # Check parameter
+        # run_tasks.py <csv file>
---
+++ project/test.py
@@ -0,0 +1,18 @@
+import unittest
+import run_tasks
+
+
+class TDDTest(unittest.TestCase):
+
+    def test_run_task_file_before(self):
+        result = run_tasks.main(['_input/test.csv',0])
+        self.assertEqualt(result, False)
+
+    def test_run_task_file_exist(self):
+        result = run_tasks.main(['_input/test.csv',0])
+        self.assertEqualt(result, True)
+
+
+
+if __name == '__main__':
+    unittest.main()
