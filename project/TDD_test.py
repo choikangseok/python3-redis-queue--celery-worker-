89e48c0166ecd558f7f950e6ec8300b56e572f98
choikangseok <ktos5427@naver.com>
2022-04-01 14:34:02 +0900

    unit_test_code

---
+++ project/TDD_test.py
@@ -0,0 +1,36 @@
+import unittest
+import run_tasks
+import tasks
+from MyWork import *
+from celery.utils.log import get_task_logger
+
+
+class TDDTest(unittest.TestCase):
+
+    def test_run_task_file_before(self):
+        result = run_tasks.main(['run_tasks.py', '_input_file/test.csv',"0"])
+        self.assertEqual(result, True)
+
+    def test_tasks(self):
+        result = tasks.my_task_0("ObjectId(""623ab4b85d3e87467f54cb1b"")","test2_first","test2_second")
+        self.assertEqual(result,True)
+
+    def test_class_MyWork(self):
+        logger = get_task_logger(__name__)
+        mywork = MyWork("ObjectId(""623ab4b85d3e87467f54cb1b"")","test2_first","test2_second", logger)
+
+
+    #DBHandler already Test Complete
+
+        #init test
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
